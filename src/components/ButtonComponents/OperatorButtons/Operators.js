fd2c46787ce40f76e75de7d88085236bd98bab7a
Jengopockets <jengopockets@gmail.com>
2019-07-10 17:24:49 -0400

    OperatorButton Dispalayed

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import OperatorButton from './OperatorButton';
+import { operators } from '../../../data';
 
 //Import your array data to from the provided data file
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorArray, setOperator]= useState(operators)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operatorArray.map(operate => <OperatorButton operate={operate} key={operate}/>)}
     </div>
   );
 };
+export default Operators;
