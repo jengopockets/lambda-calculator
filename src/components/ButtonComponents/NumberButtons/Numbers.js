e5f02205aa731bef52696a0f0f76c84a93bcd023
Jengopockets <jengopockets@gmail.com>
2019-07-10 15:16:08 -0400

    Exported Data, imported numbers

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 //import any components needed
-
+import NumberButton from './NumberButton'
 //Import your array data to from the provided data file
+import { numbers } from "../../../data"
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
