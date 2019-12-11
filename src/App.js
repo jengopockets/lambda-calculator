4de033ef1dbf69b8b76dc6ea415200699976f722
Jengopockets <jengopockets@gmail.com>
2019-12-11 11:29:37 -0500

    changed hook keywords

--- src/App.js
+++ src/App.js
@@ -17,19 +17,19 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
-  const [calcValue, setValue] = useState('0')
+  const [value, setValue] = useState('0')
 
   const clickHandler = btnClicked => {
     btnClicked === 'C' ? setValue('0'):
-    btnClicked === '=' ? setValue(evaluate(calcValue)):
-    calcValue === '0' ? setValue(btnClicked):
-    setValue(calcValue + btnClicked);
+    btnClicked === '=' ? setValue(evaluate(value)):
+    value === '0' ? setValue(btnClicked):
+    setValue(value + btnClicked);
   }
 
   return (
     <div className="container">
       <Logo />
-      <Display calcValue={calcValue}/>
+      <Display value={value}/>
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
           <Numbers clickHandler={clickHandler}/>
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Display = (props) => {
   return <div>{/* Display any props data here */}
-  <input type="text" value={props.calcValue} maxLength="14" disabled />
+  <input type="text" value={props.value} maxLength="14" disabled />
   </div>;
 };
 export default Display;
